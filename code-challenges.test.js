30ac45750b376d9dcfd358ca904b47f5792677de
Goran SD <g.cvetic23@gmail.com>
2022-09-17 14:06:34 -0700

    week 2 assessment complete

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,46 +14,153 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const numbersArray1 = [6, 7, 8, 9, 10]
+// const numbersArray1 = [6, 7, 8, 9, 10]
 // Expected output: [18, 21, 24, 27, 30]
-const numbersArray2 = [24, 27, 30, 33, 36]
+// const numbersArray2 = [24, 27, 30, 33, 36]
 // Expected output: [72, 81, 90, 99, 108]
 
 // b) Create the function that makes the test pass.
+
+// ===================================== CHALLENGE #1 =================================================
 
 // --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const object1 = { number: 15 }
-// Expected output: "15 is divisible by three"
-const object2 = { number: 0 }
-// Expected output: "0 is divisible by three"
-const object3 = { number: -7 }
-// Expected output: "-7 is not divisible by three"
+describe("isDivisibleByThree", () => {
+  it("the function will take in an object and check if te number inside is divisible by 3", () => {
+    const object1 = { number: 15 };
+    // Expected output: "15 is divisible by three"
+    const object2 = { number: 0 };
+    // Expected output: "0 is divisible by three"
+    const object3 = { number: -7 };
+    // Expected output: "-7 is not divisible by three"
+    expect(isDivisibleByThree({ number: 15 })).toEqual(
+      "15 is divisible by three"
+    );
+    expect(isDivisibleByThree({ number: 0 })).toEqual(
+      "0 is divisible by three"
+    );
+    expect(isDivisibleByThree({ number: -7 })).toEqual(
+      "-7 is not divisible by three"
+    );
+  });
+});
+
+//ReferenceError: isDivisibleByThree is not defined
 
 // b) Create the function that makes the test pass.
+
+// ============== <<<<< PSEUDO CODE >>>>> ===========================
+// we need a function that will take one argument (in our case that is an object).
+// have in mind that since the parameter is object, we need to do use (.)dot notation in order to access specific variable.
+// we are going to need a conditional statement to check if the variable passed is divisible by 3. The division check will utilize mod operation
+// since we have only two possible outcomes, one IF statement should suffice
+// the return value will use template literals to utilize dynamic implementation (${expression/variable} as a place holder) and presentation of desired string (`string wrapped in backticks`). ***Saying string interpolation would not be incorrect statement per se, just not precise enough***.
+
+const isDivisibleByThree = (object) => {
+  if (object.number % 3 == 0) {
+    return `${object.number} is divisible by three`;
+  } else {
+    return `${object.number} is not divisible by three`;
+  }
+};
+
+// test passed
+
+// ===================================== CHALLENGE #2 =================================================
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
-// Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
-const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
-// Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
+describe("allCaps", () => {
+  it("The function takes in an array of words and returns an array with all the words capitalized", () => {
+    const randomNouns1 = [
+      "streetlamp",
+      "potato",
+      "teeth",
+      "conclusion",
+      "nephew",
+    ];
+    // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+    const randomNouns2 = ["temperature", "database", "chopsticks", "mango"];
+    // Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
+    expect(allCaps(randomNouns1)).toEqual([
+      "Streetlamp",
+      "Potato",
+      "Teeth",
+      "Conclusion",
+      "Nephew",
+    ]);
+    expect(allCaps(randomNouns2)).toEqual([
+      "Temperature",
+      "Database",
+      "Chopsticks",
+      "Mango",
+    ]);
+  });
+});
+
+// ReferenceError: allCaps is not defined
 
 // b) Create the function that makes the test pass.
+
+// ============== <<<<< PSEUDO CODE >>>>> ===========================
+// we need a function that takes one parameter. In this scenario that is an array with string elements.
+// we need to iterate through the array and check on each element. For this we are going to use .map function since it returns new array be default.
+// out of every element (string) we need to access the very firs element (index of 0 of [0]) and call .toUpperCase method on that element/value alone
+// final step is adding the rest of the characters using .slice method
+
+const allCaps = (array) => {
+  return array.map((e) => {
+    return e[0].toUpperCase() + e.slice(1);
+  });
+};
+
+//test passed
+
+// ===================================== CHALLENGE #3 =================================================
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const vowelTester1 = "learn"
-// Expected output: 1
-const vowelTester2 = "academy"
-// Expected output: 0
-const vowelTester3 = "challenges"
-// Expected output: 2
+describe("firstVowel", () => {
+  it("The function that takes in a string and logs the index of the first vowel", () => {
+    const vowelTester1 = "learn";
+    // Expected output: 1
+    const vowelTester2 = "academy";
+    // Expected output: 0
+    const vowelTester3 = "challenges";
+    // Expected output: 2
+    expect(firstVowel(vowelTester1)).toEqual(1);
+    expect(firstVowel(vowelTester2)).toEqual(0);
+    expect(firstVowel(vowelTester3)).toEqual(2);
+  });
+});
+
+//ReferenceError: firstVowel is not defined
 
 // b) Create the function that makes the test pass.
+
+// ============== <<<<< PSEUDO CODE >>>>> ===========================
+// we need a function that takes in one parameter. In this case that will be a string
+// we are going to assign the string to our local variable "stringArray" by using .split method
+// once we have an array, we can utilize FOR loop and its index notation. The index notation (i) will be useful in determining the exact address/index of our string element
+// a conditional statement (IF) will check each string element/character/value until it finds one of the vowels. At that point, for loop breaks and returns the index (i)
+
+const firstVowel = (string) => {
+  const stringArray = string.split("");
+  for (i = 0; i < stringArray.length; i++) {
+    if (
+      stringArray[i] === "a" ||
+      stringArray[i] === "e" ||
+      stringArray[i] === "o" ||
+      stringArray[i] === "u" ||
+      stringArray[i] === "i"
+    ) {
+      return i;
+    }
+  }
+};
--- coding-concepts.js
+++ coding-concepts.js
@@ -7,62 +7,60 @@
 
 // --------------------1) What will this log?
 
-const cohort = "Foxtrot 2022"
+const cohort = "Foxtrot 2022";
 // console.log(cohort.split(""))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: ['F', 'o', 'x', 't', 'r', 'o', 't', ' ','2', '0', '2', '2']
+// b) Verify and explain: variable cohort holds a string value in its memory. When we call the method .split on "cohort", it is going to "convert" every element/character of the string into an individual array element. Including empty spaces.
 
 // --------------------2) What will this log?
 
 const greeter = (name) => {
-  `Hello, ${name}!`
-}
+  `Hello, ${name}!`;
+};
 // console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: undefined
+// b) Verify and explain: the function "greeter" produces no return. Missing the "return" before template literal.
 
 // --------------------3) What will this log?
 
-const multipliedByTwo = [4, 5, 6, 7, 8].map((number) => number * 2)
+const multipliedByTwo = [4, 5, 6, 7, 8].map((number) => number * 2);
 // console.log(multipliedByTwo)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: [ 8, 10, 12, 14, 16 ]
+// b) Verify and explain: when we call the function assigned to the variable multipliedByTwo, .map method will return, by default, an array with the values of the initial array multiplied by two.
 
 // --------------------4) What will this log?
 
-const onlyOdds = [11, 12, 13, 14, 15].filter((number) => number % 2 !== 0)
+const onlyOdds = [11, 12, 13, 14, 15].filter((number) => number % 2 !== 0);
 // console.log(onlyOdds)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: [11,13,14]
+// b) Verify and explain: when we call the function assigned to the variable onlyOdds, .filter method will return, by default, an array with the values that satisfy/pass the conditional. The conditional checks if the number is odd. We are using mod operation to accomplish this.
 
 // --------------------5) What will this log?
 
-const myCodingSkills = {
-  languages: ["JavaScript", "Ruby"],
-  frameworks: ["React", "Ruby on Rails"],
-  databases: "PostgreSQL",
-  versionControl: "GitHub"
-}
-// console.log(myCodingSkills.languages[0])
+// const myCodingSkills = {
+//   languages: ["JavaScript", "Ruby"],
+//   frameworks: ["React", "Ruby on Rails"],
+//   databases: "PostgreSQL",
+//   versionControl: "GitHub",
+// };
+// console.log(myCodingSkills.languages[0]);
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: JavaScript
+// b) Verify and explain: console.log will call the object myCodingSkills, and using dot notation, it will locate the variable - "languages". Once there, we are calling an element with index of "0", which holds the string value of JavaScript.
 
 // --------------------STRETCH: What will this log?
 
 class Learn {
   constructor(name) {
-    this.student = name,
-    this.cohort = "Foxtrot",
-    this.year = 2022
+    (this.student = name), (this.cohort = "Foxtrot"), (this.year = 2022);
   }
 }
-const learnStudent = new Learn("George")
-// console.log(learnStudent)
+const learnStudent = new Learn("George");
+console.log(learnStudent);
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Learn { student: 'George', cohort: 'Foxtrot', year: 2022 }
+// b) Verify and explain: at the point of declaring/assigning the class Learn, the constructor method takes in one parameter -"name", which is used to set the initial value of the field this.student. The method only declares the variable student(it's undefined at this point), while declaring and assigning variables for cohort and year . When we declare and assign our new variable learnStudent we pass in an argument "George" into our new object. Now, the only place this argument can go, as previously determined by the constructor method, is to be assigned to the variable student. At this point consol.log will print our new object with 3 variables and their assigned values.
